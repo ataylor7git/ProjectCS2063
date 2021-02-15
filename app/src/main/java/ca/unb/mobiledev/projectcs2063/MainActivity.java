dd40a9a9b5e416c67ac16f7e8534a5a3bef03b10
mahletl <mlegesse@unb.ca>
2021-02-15 03:12:40 -0400

    Revert "-did a bottom navigation panel instead of the three buttons"
    This reverts commit efb2d32af0cd868ecf0e2fe63e08ca98d6747c8b.

--- app/src/main/java/ca/unb/mobiledev/projectcs2063/MainActivity.java
+++ app/src/main/java/ca/unb/mobiledev/projectcs2063/MainActivity.java
@@ -1,63 +1,14 @@
 package ca.unb.mobiledev.projectcs2063;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG = "INFO Main Activity";
-
-    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "on create called");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        openFragment(WaterFragment.newInstance("", ""));
     }
-
-        public void openFragment(Fragment fragment) {
-            Log.i(TAG, "open fragment called");
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.container, fragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
-
-        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Log.i(TAG, "listener actions called");
-
-                        switch (item.getItemId()) {
-                            case R.id.water:
-                                openFragment(WaterFragment.newInstance("", ""));
-                                return true;
-                            case R.id.steps:
-                                openFragment(StepsFragment.newInstance("", ""));
-                                return true;
-                            case R.id.user:
-                                openFragment(UserFragment.newInstance("", ""));
-                                return true;
-                        }
-                        return false;
-                    }
-                };
-    }
+}
